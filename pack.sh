c17781513ccbf0a04cf30e1cfc421378ad6dc331
moye <dkyaorui@163.com>
2020-04-29 17:30:09 +0800

    add pack.sh

--- pack.sh
+++ pack.sh
@@ -2,16 +2,23 @@
 
 echo "start pack vcode..."
 
-mvn package
+mvn clean package
 
-echo "start build docker image"
+echo "Start build docker images..."
+echo "Login docker registry"
+docker login --username=$1 registry.cn-hangzhou.aliyuncs.com
 
 # shellcheck disable=SC2046
-tag=$(git describe --tags $(git rev-list --tags --max-count=1))
-echo "get the newest tag: ${tag}"
+gitTag=$(git describe --tags $(git rev-list --tags --max-count=1))
+echo "get the newest tag: ${gitTag}"
+dockerTag=$(echo ${gitTag} | egrep '(v[0-9]*\.[0-9]*\.[0-9]*)' -o)
 
-# build docker
-echo "start build docker..."
-docker build --tag vcode:"${tag}" -f ./Dockerfile .
+echo "build images..."
+docker build --tag registry.cn-hangzhou.aliyuncs.com/vcodeteam/vcode:${dockerTag} -f ./Dockerfile .
+docker tag $(docker image ls -q registry.cn-hangzhou.aliyuncs.com/vcodeteam/vcode:${dockerTag}) registry.cn-hangzhou.aliyuncs.com/vcodeteam/vcode:latest
+
+echo "push images..."
+docker push registry.cn-hangzhou.aliyuncs.com/vcodeteam/vcode:${dockerTag}
+docker push registry.cn-hangzhou.aliyuncs.com/vcodeteam/vcode:latest
 
 echo "pack vcode-vcode docker image success..."
